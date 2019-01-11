c50fcba6b829db36564c1a81bc785e67f9442890
xiangpeng93 <1005110192@qq.com>
2019-01-11 15:06:31 +0800

    修改加班页面与后台

--- operateWorkOverTimeXls.py
+++ operateWorkOverTimeXls.py
@@ -15,8 +15,8 @@ def open_excel(file):
         print str(e)
 def _transValueToDate(value):
     try:
-        date = datetime(*xlrd.xldate_as_tuple(int(value), 0))
-        return date.strftime('%Y-%m-%d:%H-%M-%S')
+        date = datetime(*xlrd.xldate_as_tuple(float(value), 0))
+        return date.strftime('%Y-%m-%d %H:%M:%S')
     except Exception,error:
         pass
         #print "_transValueToDate error",error
@@ -49,24 +49,29 @@ def ProcessWorkOvertimeXLS(fileName):
             nCols = sheet.ncols
             if nRows > 0 and nCols>0:
                 for row in range(g_nStartRow,nRows):
-                    rowValue = sheet.row_values(row)   
-                    rowValue[4] = _transValueToDate(rowValue[4])
-                    rowValue[5] = _transValueToDate(rowValue[5])
-                    for i in range(0,len(rowValue)):
-                        rowValue[i] = _transFloatToInt(rowValue[i])
-                    if rowValue[8] == "on":
-                        rowValue[8] = 1
-                    else :
-                        rowValue[8] = 0
+                    ## 增加容错率
+                    try:
+                        rowValue = sheet.row_values(row)   
+                        rowValue[4] = _transValueToDate(rowValue[4])
+                        rowValue[5] = _transValueToDate(rowValue[5])
+                        for i in range(0,len(rowValue)):
+                            rowValue[i] = _transFloatToInt(rowValue[i])
+                        if rowValue[8] == "on":
+                            rowValue[8] = u"调休"
+                        else :
+                            rowValue[8] = u"非调休"
+                            
+                        tData = (rowValue[0],rowValue[1],rowValue[2],rowValue[3],rowValue[4],
+                                 rowValue[5],rowValue[6],rowValue[7],rowValue[8])
                         
-                    tData = (rowValue[0],rowValue[1],rowValue[2],rowValue[3],rowValue[4],
-                             rowValue[5],rowValue[6],rowValue[7],rowValue[8])
-                    
-                    if OvertimeWorkData.has_key(rowValue[0]):
+                        if OvertimeWorkData.has_key(rowValue[0]):
+                            pass
+                        else:
+                            OvertimeWorkData[rowValue[0]] = []
+                        OvertimeWorkData[rowValue[0]].append(tData)
+                    except:
+                        print "parse ProcessWorkOvertimeXLS row value error",error
                         pass
-                    else:
-                        OvertimeWorkData[rowValue[0]] = []
-                    OvertimeWorkData[rowValue[0]].append(tData)
                 print nRows,nCols
         except Exception,error:
             print "error",error
@@ -74,34 +79,50 @@ def ProcessWorkOvertimeXLS(fileName):
         finally:
             pass
 def GetWorkoverData():
+    if len(OvertimeWorkData) == 0:
+        ProcessWorkOvertimeXLS("overtime.xlsx")
     print u"获取加班信息"
     DstInfo = [];
+    perDayCost = 139
+    perHourCost = 18
     for item in OvertimeWorkData:
-        days = 0
-        hours = 0
-        leaves = 0
-        
-        for info in OvertimeWorkData[item]:
-            tDataDict = {}
-            days = days + info[6]
-            hours = hours + info[7]
-            leaves = leaves + info[8]
-            tDataDict["name"] = info[0]
-            tDataDict["comp"] = info[1]
-            tDataDict["proj"] = info[2]
-            tDataDict["job"] = info[3]
-            tDataDict["startTime"] = info[4]
-            tDataDict["endTime"] = info[5]
-            tDataDict["days"] = info[6]
-            tDataDict["hours"] = info[7]
-            tDataDict["leaves"] = info[8]
-            DstInfo.append(tDataDict)
-        tDataAll = {}
-        tDataAll["name"] = u"合计"
-        tDataAll["days"] = days
-        tDataAll["hours"] = hours
-        tDataAll["leaves"] = leaves
-        DstInfo.append(tDataAll)
+        ## 增加容错率
+        try:
+            days = 0
+            hours = 0
+            leaves = 0
+            realDays = 0
+            realHours = 0
+            for info in OvertimeWorkData[item]:
+                tDataDict = {}
+                days = days + int(info[6])
+                hours = hours + int(info[7])
+                tDataDict["name"] = info[0]
+                tDataDict["comp"] = info[1]
+                tDataDict["proj"] = info[2]
+                tDataDict["job"] = info[3]
+                tDataDict["startTime"] = info[4]
+                tDataDict["endTime"] = info[5]
+                tDataDict["days"] = info[6]
+                tDataDict["hours"] = info[7]
+                tDataDict["leaves"] = info[8]
+                if tDataDict["leaves"] == u"非调休":
+                    realDays = realDays + int(info[6])
+                    realHours = realHours + int(info[7])
+                DstInfo.append(tDataDict)
+            tDataAll = {}
+            tDataAll["name"] = item + u" 加班统计:"
+            tDataAll["comp"] = OvertimeWorkData[item][0][1]
+            tDataAll["proj"] = OvertimeWorkData[item][0][2]
+            tDataAll["job"] = u"总天数:" + str(days)
+            tDataAll["startTime"] = u"总小时数:"+ str(hours)
+            tDataAll["endTime"] = u"实际天数:"+ str(realDays)
+            tDataAll["days"] = u"实际小时数:"+ str(realHours)
+            tDataAll["hours"] = u"加班工资:"+ str(realDays*perDayCost+realHours*perHourCost)
+            DstInfo.append(tDataAll)
+        except:
+            print "parse GetWorkoverData row value error",error
+            pass
     OvertimeWorkData.clear()
     return DstInfo
 ##ProcessWorkOvertimeXLS("test.xlsx")
