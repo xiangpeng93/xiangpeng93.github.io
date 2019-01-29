97814c8a3f85d831318f8540d29199c30eb909d3
xiangpeng93 <1005110192@qq.com>
2019-01-29 14:50:30 +0800

    修改ip地址

--- src/link.js
+++ src/link.js
@@ -1,5 +1,5 @@
 export default {
-  'host': 'http://58.101.89.27:9608',
+  'host': 'http://58.101.95.75:9608',
   //'host': 'http://127.0.0.1:9608',
   'loginPath': '/login',
   'uploadHomePic':'/uploadHomePic'
