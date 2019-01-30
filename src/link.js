eecb7781dbf59b370daaa90f12adc02200873818
xiangpeng93 <1005110192@qq.com>
2019-01-30 14:13:45 +0800

    使用阿里云

--- src/link.js
+++ src/link.js
@@ -1,5 +1,5 @@
 export default {
-  'host': 'http://58.101.95.75:9608',
+  'host': 'http://funmaker.fun:9608',
   //'host': 'http://127.0.0.1:9608',
   'loginPath': '/login',
   'uploadHomePic':'/uploadHomePic'
